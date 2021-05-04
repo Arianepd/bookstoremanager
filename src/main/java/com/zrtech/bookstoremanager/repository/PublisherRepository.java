c5a2f347917668f23fdf5591b6e44d35110c1de0
Ariane Coelho <arianeclh09@gmail.com>
2021-05-04 20:52:35 -0300

    Criação do Repositórios

---
+++ src/main/java/com/zrtech/bookstoremanager/repository/PublisherRepository.java
@@ -0,0 +1,9 @@
+package com.zrtech.bookstoremanager.repository;
+
+import com.zrtech.bookstoremanager.entities.Author;
+import com.zrtech.bookstoremanager.entities.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+}
---
+++ src/main/java/com/zrtech/bookstoremanager/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.zrtech.bookstoremanager.repository;
+
+import com.zrtech.bookstoremanager.entities.Author;
+import com.zrtech.bookstoremanager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+}
